669bd0d19f4c2f0da39b1c0ddfe04215e5e0ce4e
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-04 04:26:25 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+//npm import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const cardsGallery = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
