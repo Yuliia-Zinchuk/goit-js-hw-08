177f18edb617ed926b339cbb4d91a8ec04fefe1b
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-04 06:16:11 +0200

    add player with npm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
