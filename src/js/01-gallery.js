638d2e5fcddba0098a3c9d4a622c9481458dfb84
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-05 14:41:12 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,10 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Описан в документации
+
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
 
 console.log(galleryItems);
-
-//npm import { galleryItems } from './gallery-items.js';
 
 const galleryContainer = document.querySelector('.gallery');
 
