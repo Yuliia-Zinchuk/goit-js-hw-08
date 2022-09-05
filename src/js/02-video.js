5966051f5fb2960529015c2e2e99dc2bdbaab4bb
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-05 14:41:24 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,20 +6,9 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 function onPlay(data) {
-  // console.log(data.seconds);
-  //console.log(JSON.stringify(data));
-
-  // const stringData = JSON.stringify(data);
-  //console.log(stringData);
-
-  //   const objgData = JSON.parse(stringData);
-  //   console.log(objgData);
   localStorage.setItem('videoplayer-current-time', data.seconds);
-  //console.log(localStorage.getItem('videoplayer-current-time'));
 }
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const stopTimeLast = localStorage.getItem('videoplayer-current-time');
-
-player.setCurrentTime(stopTimeLast);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
