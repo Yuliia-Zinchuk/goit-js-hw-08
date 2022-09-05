c2f92f9bb1cc8b0be8d8a47c793fe1724b5b645d
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-05 13:57:13 +0200

    Add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const textarea = document.querySelector('[name="message"]');
+const inputEmail = document.querySelector('[name="email"]');
+
+const formData = {};
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(evt => {
+    const {
+      elements: { email, message },
+    } = evt.currentTarget;
+
+    formData.email = email.value;
+    formData.message = message.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }),
+  500
+);
+
+populateMessageOutput();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+  event.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function populateMessageOutput() {
+  const json = localStorage.getItem('feedback-form-state');
+
+  const savedMessage = JSON.parse(json);
+  if (savedMessage) {
+    textarea.value = savedMessage.message;
+    inputEmail.value = savedMessage.email;
+  }
+}
